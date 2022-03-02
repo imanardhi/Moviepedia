0d9bf67048797715c508aeb08c8e3d27fe556d22
Iman Ardhi Prabowo <imanardhiprabowo@gmail.com>
2022-03-02 12:12:59 +0700

    Integrate ui with dagger #2

--- app/src/main/java/com/imanardhi/moviepedia/view/MainActivity.kt
+++ app/src/main/java/com/imanardhi/moviepedia/view/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.imanardhi.moviepedia.view
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.imanardhi.moviepedia.R
+import com.imanardhi.moviepedia.databinding.ActivityMainBinding
+import dagger.android.support.DaggerAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
     }
 }
